e5e0924b4e508db32ce37de6c762903e5fcf21e6
maxcomfort <max.d.comfort@outlook.com>
2023-09-23 09:52:53 -0700

    Updating libraries

--- libraries/RTClib/examples/softrtc/softrtc.ino
+++ libraries/RTClib/examples/softrtc/softrtc.ino
@@ -1,65 +1,45 @@
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
-#include <Wire.h>
+// Date and time functions using just software, based on millis() & timer
+
 #include "RTClib.h"
 
-#if defined(ARDUINO_ARCH_SAMD)
-// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
-   #define Serial SerialUSB
-#endif
-
-RTC_DS1307 rtc;
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+RTC_Millis rtc;
 
 void setup () {
+    Serial.begin(57600);
 
 #ifndef ESP8266
-  while (!Serial); // for Leonardo/Micro/Zero
+    while (!Serial); // wait for serial port to connect. Needed for native USB
 #endif
 
-  Serial.begin(57600);
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
 }
 
 void loop () {
     DateTime now = rtc.now();
-    
+
     Serial.print(now.year(), DEC);
     Serial.print('/');
     Serial.print(now.month(), DEC);
     Serial.print('/');
     Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
+    Serial.print(' ');
     Serial.print(now.hour(), DEC);
     Serial.print(':');
     Serial.print(now.minute(), DEC);
     Serial.print(':');
     Serial.print(now.second(), DEC);
     Serial.println();
-    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-    
+
+    Serial.print(" seconds since 1970: ");
+    Serial.println(now.unixtime());
+
     // calculate a date which is 7 days and 30 seconds into the future
-    DateTime future (now + TimeSpan(7,12,30,6));
-    
+    DateTime future (now.unixtime() + 7 * 86400L + 30);
+
     Serial.print(" now + 7d + 30s: ");
     Serial.print(future.year(), DEC);
     Serial.print('/');
@@ -73,7 +53,7 @@ void loop () {
     Serial.print(':');
     Serial.print(future.second(), DEC);
     Serial.println();
-    
+
     Serial.println();
     delay(3000);
 }
